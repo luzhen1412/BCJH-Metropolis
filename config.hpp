840739530a9d4d77eb4b0716f12b7dc75bef7696
luzhen1412 <279790928@qq.com>
2023-06-16 22:34:01 +0800

    Update config.hpp

--- config.hpp
+++ config.hpp
@@ -7,7 +7,7 @@
 #define NUM_CHEFS (NUM_GUESTS * CHEFS_PER_GUEST)
 
 #define MODE 1 // 1:宴会模式; 2:限时任务
-#define TARGET_SCORE_APPROXIMATE 1980000
+#define TARGET_SCORE_APPROXIMATE 2360000
 // 这里输分数线
 // 如果是宴会模式，不用特别准，差两倍/一半以内都可以
 
@@ -16,7 +16,7 @@
 
 #define AVOID_CHEF_1 true
 #define AVOID_CHEF_2 true
-#define AVOID_CHEF_3 true // 计算时不考虑三火厨师
+#define AVOID_CHEF_3 false // 计算时不考虑三火厨师
 
 // ************************************************** //
 // ** 以下内容不建议修改，除非你清楚地知道这是什么意思 ** //
